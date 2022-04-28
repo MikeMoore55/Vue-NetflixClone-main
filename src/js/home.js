770d5b6ec5c40de98eba110d23a7b88d302c49c4
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-04-28 16:53:32 +0200

    added 6 demo movies to array for testing
    added 6 demo movies to array for testing

--- src/js/home.js
+++ src/js/home.js
@@ -22,8 +22,64 @@ const component = {
       return {
         movieList:[
           new Movie(
-            
-          )
+            "pi8HsKXN5G",
+            "Everest",
+            "Adventure",
+            "false",
+            "18/09/2015",
+            "/src/images/Everest",
+            "https://www.youtube.com/watch?v=5ZQVpPiOji0"
+          ),
+
+          new Movie(
+            "zhfzHGhFjK",
+            "The Fast and The Furious",
+            "Action",
+            "false",
+            "22/06/2001",
+            "/src/images/Fast-and-Furious-1",
+            "https://www.youtube.com/watch?v=GoSUdZJg3pw"
+          ),
+
+          new Movie(
+            "TymNNADk_X",
+            "2 Fast 2 Furious",
+            "Action",
+            "false",
+            "06/06/2003",
+            "/src/images/Fast-and-Furious-2",
+            "https://www.youtube.com/watch?v=ZZGkV_xWGw4",
+          ),
+
+          new Movie(
+            "N7xyWhg0i",
+            "Fast and Furious",
+            "Action",
+            "false",
+            "12/03/2009",
+            "/src/images/Fast-and-Furious-4",
+            "https://www.youtube.com/watch?v=BCg9R_MGZKg",
+          ),
+
+          new Movie(
+            "vdPZEOGsEj",
+            "Fast 5",
+            "Action",
+            "false",
+            "29/04/2011",
+            "/src/images/Fast-and-Furious-5",
+            "https://www.youtube.com/watch?v=OqjeOYeG5_A",
+          ),
+
+          new Movie(
+            "YS8INBNOTe",
+            "Fast and Furious 6",
+            "Action",
+            "false",
+            "06/05/2013",
+            "/src/images/Fast-and-Furious-6",
+            "https://www.youtube.com/watch?v=C_puVuHoR6o",
+          ),
 
         ],
         userArray: [],
