5b8f5965d09355d37df9fcf6c1c39beb7e0a63bb
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-04-28 18:06:19 +0200

    added images
    added images to movies
    added li component to nest img
    need to fix error

--- src/js/home.js
+++ src/js/home.js
@@ -27,7 +27,7 @@ const component = {
             "Adventure",
             "false",
             "18/09/2015",
-            "/src/images/Everest",
+            href="/src/images/Everest.jpg",
             "https://www.youtube.com/watch?v=5ZQVpPiOji0"
           ),
 
@@ -37,7 +37,7 @@ const component = {
             "Action",
             "false",
             "22/06/2001",
-            "/src/images/Fast-and-Furious-1",
+            href="/src/images/fast-and-furious-1",
             "https://www.youtube.com/watch?v=GoSUdZJg3pw"
           ),
 
@@ -47,7 +47,7 @@ const component = {
             "Action",
             "false",
             "06/06/2003",
-            "/src/images/Fast-and-Furious-2",
+            href="/src/images/fast-and-furious-2",
             "https://www.youtube.com/watch?v=ZZGkV_xWGw4",
           ),
 
@@ -57,7 +57,7 @@ const component = {
             "Action",
             "false",
             "12/03/2009",
-            "/src/images/Fast-and-Furious-4",
+            href="/src/images/fast-and-furious-4",
             "https://www.youtube.com/watch?v=BCg9R_MGZKg",
           ),
 
@@ -67,7 +67,7 @@ const component = {
             "Action",
             "false",
             "29/04/2011",
-            "/src/images/Fast-and-Furious-5",
+            href="/src/images/fast-and-furious-5",
             "https://www.youtube.com/watch?v=OqjeOYeG5_A",
           ),
 
@@ -75,9 +75,9 @@ const component = {
             "YS8INBNOTe",
             "Fast and Furious 6",
             "Action",
-            "false",
+            "true",
             "06/05/2013",
-            "/src/images/Fast-and-Furious-6",
+            href="/src/images/fast-and-furious-6",
             "https://www.youtube.com/watch?v=C_puVuHoR6o",
           ),
 
@@ -88,11 +88,18 @@ const component = {
   },
 
   methods: {
-      
+    logout() {
+      localStorage.removeItem(LOGGED_IN_USER_KEY);
+      window.location.href = "../../index.html";
+    },
   },
 
   computed:{
-
+    comingSoonList() {
+      return this.movieList.filter((movie) => {
+        return movie.comingSoon===true;
+      });
+    },
   },
 
   template: /* html */
@@ -101,17 +108,21 @@ const component = {
     <img class="netflix-logo" src="/src/images/netflix-logo-img.png">
     <div class="log-out-div">
       <p class="user-name">{{username}}</p>
-      <button class="log-out">Log out!</button>
+      <button class="log-out" @click="logout">Log out!</button>
     </div>
 
-    <div id="hero" class="hero-header">
-      <h1>Coming soon!</h1>
-      <br>
-      <br>
-      <br>
-      <br>
-      <p>upcoming movies will be displayed here soon...</p>
-
+    <div id="movieDisplay">
+      <ul>
+        <li v-for="(movie, index) in movieList" :key="movie.id">
+        <span>{{ movie.name }}</span>
+        <br/>
+        <span>{{ movie.genre }}</span>
+        <br/>
+        <span>{{ movie.availDate }}</span>
+        <br/>
+        <img class="movie-img" v-bind:src="movie.thumbnail">
+        </li>
+      </ul>
     </div>
   
   
