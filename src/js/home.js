0dd377a6371fcd7027690076b587afcfd8ce8f9f
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-05-04 15:52:15 +0200

    changes
    added...
    responsiveness
    fixed...
    watchlist issues

--- src/js/home.js
+++ src/js/home.js
@@ -156,7 +156,7 @@ const component = {
         new Movie(
           "9BFEgBKzKF",
           "Avatar 2",
-          "Action/Adventure",
+          "Adventure",
           true,
           "16/12/2022",
           "/src/images/avatar-2.jpg",
@@ -227,7 +227,7 @@ const component = {
       userArray: [],
       username: "",
       comingSoon: [],
-      watchList:[]
+      watchList: []
     }
   },
 
@@ -240,12 +240,12 @@ const component = {
     myListBtn() {
       window.location.href = "/src/pages/watch-list-page.html";
     },
-    
+
     addToWatchList(index) {
       // if WATCH_LIST_KEY is empty
       if (!localStorage.getItem(WATCH_LIST_KEY)) {
         let watchListArray = [];
-        watchListArray.push(this.availableList[index]);
+        watchListArray.push(this.availableList[index] && this.comingSoonList[index]);
         localStorage.setItem(
           WATCH_LIST_KEY,
           JSON.stringify(watchListArray)
@@ -255,7 +255,7 @@ const component = {
         let watchListArray = JSON.parse(
           localStorage.getItem(WATCH_LIST_KEY)
         );
-        watchListArray.push(this.availableList[index]);
+        watchListArray.push(this.availableList[index] && this.comingSoonList[index]);
         localStorage.setItem(
           WATCH_LIST_KEY,
           JSON.stringify(watchListArray)
@@ -281,48 +281,62 @@ const component = {
 
   template: /* html */
     `
-  
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-override fluid" aria-label="Eleventh navbar example">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img class="netflix-logo" src="/src/images/netflix-logo-img.png" /></a>
-          <button class="nav-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-            <img class="nav-icon" src="/src/images/menu-icon.png">
-          </button>
+  <nav class="nav">
+    <div class="nav-small">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-override fluid" aria-label="Eleventh navbar example">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class="netflix-logo" src="/src/images/netflix-logo-img.png" /></a>
+            <button class="nav-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+              <img class="nav-icon" src="/src/images/menu-icon.png">
+            </button>
 
-          <div class="collapse navbar-collapse nav-list-div-override" id="navbarsExample09">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-list-override">
-              <li class="nav-item nav-item-override">
-              <p class="user-name">User: {{username}}</p>
-              </li>
-              <li class="nav-item nav-item-override">
-                <button class="my-list-btn" @click="myListBtn">My Watch-List</button>
-              </li>
-              <li class="nav-item nav-item-override">
-                <button class="log-out" @click="logout">Log out!</button>
-              </li>
-            </ul>
-           
+            <div class="collapse navbar-collapse nav-list-div-override" id="navbarsExample09">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-list-override">
+                <li class="nav-item nav-item-override">
+                  <p class="user-name">User: {{username}}</p>
+                </li>
+                <li class="nav-item nav-item-override">
+                  <button class="my-list-btn" @click="myListBtn">My Watch-List</button>
+                </li>
+                <li class="nav-item nav-item-override">
+                  <button class="log-out" @click="logout">Log out!</button>
+                </li>
+              </ul>
+            
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
-
+    <div class="nav-med">
+      <img class="netflix-logo" src="/src/images/netflix-logo-img.png" />
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <p class="user-name">{{username}}</p>
+        </li>
+        <li class="nav-list-item">
+          <button class="my-list-btn" @click="myListBtn">Watch-List</button>
+        </li>
+        <li class="nav-list-item">
+          <button class="log-out" @click="logout">Log out!</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
  
-
     <br>
     <br>
     <br>
     <br>
-
 
     <header id="comingSoon-movie" class="comingSoon-movie">
 
       <h2>coming soon</h2>
 
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="comingSoon-small">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
           
-
           <div class="carousel-inner">
               <template v-for="(movie, index) in comingSoonList">
                   <div :class="{ 'carousel-item': true, active: index === 0 }" >
@@ -330,8 +344,11 @@ const component = {
 
                       <h3>{{movie.name}}</h3>
                       <p class="movie-info">{{movie.genre}}</p>
-                      <p class="movie-info">{{movie.availDate}}</p>
-                      <button class="play-btn"><img class="play-icon" src="/src/images/play-icon.png"></button>
+                      <p class="movie-info">
+                        <button class="play-btn"><img class="play-icon" src="/src/images/play-icon.png"></button>
+                        {{movie.availDate}}
+                        <button class="add-btn" @click="addToWatchList(index)"><img class="add-icon" src="/src/images/add-icon.png"></button>  
+                      </p>
                   </div>
                   
               </template>
@@ -346,8 +363,21 @@ const component = {
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
+        </div>
       </div>
-
+      <div class="comingSoon-med">
+        <ul class="comingSoon-list-med">
+          <li class="comingSoon-movie-item-med" v-for="(movie, index) in comingSoonList" :key="movie.id">
+            <span class="movie-name">{{ movie.name }}</span>
+            <br/>
+            <img class="movie-img" v-bind:src="movie.thumbnail">
+            <br/>
+            <span class="movie-info">{{ movie.genre }}</span>
+            <br/>
+            <span class="movie-info">{{ movie.availDate }}</span>
+          </li>
+          </ul>
+      </div>
     </header>
 
     <br>
@@ -357,34 +387,55 @@ const component = {
     <section id="available-movie" class="available-movie">
         
       <h2>movies</h2>
-
-      <div id="carouselExampleCaptions1" class="carousel slide" data-bs-ride="carousel">
+      <div class="available-small">
+        <div id="carouselExampleCaptions1" class="carousel slide" data-bs-ride="carousel">
         
-
-        <div class="carousel-inner">
+          <div class="carousel-inner">
             <template v-for="(movie, index) in availableList">
                 <div :class="{ 'carousel-item': true, active: index === 0 }" >
                     <img :src="movie.thumbnail" class="d-block  w-50 h-50 movie-img" :alt="movie.name">
                     <h3>{{ movie.name }}</h3>
                     <p class="movie-info">{{movie.genre}}</p>
-                    <p class="movie-info">{{movie.availDate}}</p>
-                    <button class="add-btn" @click="addToWatchList(index)"><img class="add-icon" src="/src/images/add-icon.png"></button>  
-                </div>
+                    <p class="movie-info">
+                      <button class="play-btn"><img class="play-icon" src="/src/images/play-icon.png"></button>
+                      {{movie.availDate}}
+                      <button class="add-btn" @click="addToWatchList(index)"><img class="add-icon" src="/src/images/add-icon.png"></button>  
+                    </p>
+                  </div>
             </template>
-        </div>
+          </div>
         
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
 
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
+      <div class="available-med">
+        <ul class="available-list-med">
+          <li class="available-movie-item-med" v-for="(movie, index) in availableList" :key="movie.id">
+            <span class="movie-name">{{ movie.name }}</span>
+            <br/>
+            <img class="movie-img" v-bind:src="movie.thumbnail">
+            <br/>
+            <span class="movie-info">{{ movie.genre }}</span>
+            <br/>
+            <span class="movie-info">{{ movie.availDate }}</span>
+          </li>
+          </ul>
       </div>
 
     </section>
+
+  <br>
+  <br>
+  <br>
 
     <footer>
         <div class="footer-grid">
